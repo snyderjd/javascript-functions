7ebd24239a47100a0dff8f44ca9564ef85101775
snyder <snyderjosephd@yahoo.com>
2019-07-15 10:36:30 -0500

    Added chickenMonkey function

---
+++ scripts/chicken.js
@@ -0,0 +1,21 @@
+// Write a program that prints the numbers from 1 to 100. But for multiples of five(5, 10, 15, etc.) print "Chicken" instead of the number and for the multiples of seven(7, 14, 21, etc.) print "Monkey".For numbers which are multiples of both five and seven print "ChickenMonkey".
+
+// To determine if a number can be evenly divided by 5 or 7, use the JavaScript remainder operator.
+
+// You can start with this simpler version that outputs even numbers to the console and ignores odd numbers.You need to refactor this code to meet the requirements above.
+
+const chickenMonkey = function() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 5 === 0 && i % 7 === 0) {
+            console.log("ChickenMonkey");
+        } else if (i % 5 === 0) {
+            console.log("Chicken");
+        } else if (i % 7 === 0) {
+            console.log("Monkey");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+chickenMonkey();
