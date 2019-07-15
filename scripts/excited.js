bfca1e2b32377719474a5123b6b6cfc5328269b3
snyder <snyderjosephd@yahoo.com>
2019-07-15 10:59:18 -0500

    Completed overly excited function exercise

---
+++ scripts/excited.js
@@ -0,0 +1,9 @@
+// For this exercise, you need to create a single JavaScript function named addExcitement that will use console.log() to print out a sentence to the browser console.
+
+function addExcitement(sentence) {
+    console.log(sentence);
+}
+
+const mySentence = "Today is Monday, July 15th."
+
+// addExcitement(mySentence);
